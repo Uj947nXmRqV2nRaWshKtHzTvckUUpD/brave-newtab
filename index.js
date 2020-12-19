6eac0c1fd7318588e3fdd2bdd38b8e3dcb37cf9f
Futur Fusionneur <fusionneur@yahoo.com>
2020-12-20 01:19:44 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,16 +5,21 @@ let dom = document.getElementById("bgimg");
 (function () {
     const categories = ["inspire", "life", "funny", "love", "art", "party", "summer", "technology", "nature", "dream", "space", "inspirational", "motivational", "photography", "animals", "alpaca", "game", "music", "girl", "sexy", "flower", "tribal", "star", "chill", "relax", "coffee", "bird", "mountain", "aurora", "portrait", "joke", "event", "fashion", "travel", "architecture", "pet", "sports", "aerial", "science", "stock", "crystal", "gem", "cute", "piano", "drums", "guitar", "electro", "cat", "dog", "sweet", "landscape", "minimalist", "thinkins", "success", "statue", "young", "diva", "woman", "vr", "freedom", "stylish", "colourful", "futuristic", "abstract", "brain", "human", "skyline", "sunrise", "sunset", "greece", "italy", "food", "healthy", "convertible", "couple", "shoes", "sheep", "laugh", "smile", "happy", "child", "outdoor", "plane", "cosplay", "costume", "peace", "countryside", "tuscany", "leaves", "africal", "artificial", "intelligence", "family", "woods", "wireless", "city", "hands", "theater", "cinema", "movie", "retriever", "wedding", "teacher", "road", "gun", "wild", "lips", "jump", "wolf", "picture", "car", "sportscar", "legacy", "rocket", "saturn", "galaxy", "sun", "royal", "rock", "cartoon", "beach", "palm", "skyscraper", "graffiti", "spider", "butterfly", "kiss", "kid", "spaceship", "robot", "motorcycle", "blonde", "malta", "los angeles", "bike", "perfect-body"]; //curated list of categories
     //const categories = ["winter", "autumn", "snow", "business", "management", "universe" ]; //extra categories
+
     //const photoTypes = ["random", "featured"]
     //const photoTypeNumber = Math.floor(Math.random() * photoTypes.length);
     //const photoType = photoTypes[photoTypeNumber]; //featured photos are more cool than random ones
+
     const photoType = "featured";
     //const photoType = "random";
+
     const randomCategoryNumber = Math.floor(Math.random() * categories.length);
     const randomCategory = categories[randomCategoryNumber];
+
     //console.log("screen.resolution: " + screen.width + 'x' + screen.height);
-    const promisedImage = fetch('https://source.unsplash.com/' + photoType + '/' + screen.width + 'x' + screen.height) //no topic - much faster
-    //const promisedImage = fetch('https://source.unsplash.com/' + photoType + '/' + screen.width + 'x' + screen.height + '?' + randomCategory) //include specific search keywords
+
+    //const promisedImage = fetch('https://source.unsplash.com/' + photoType + '/' + screen.width + 'x' + screen.height) //no topic - much faster ; since 19 Dec 2020 - it's broke
+    const promisedImage = fetch('https://source.unsplash.com/' + photoType + '/' + screen.width + 'x' + screen.height + '?' + randomCategory) //include specific search keywords
         .then(response => response)
         .then((imageObj) => {
         //console.log('in async');
